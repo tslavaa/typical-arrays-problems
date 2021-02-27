f4d97cbbc1bf671d9b9dc9c5f0e25f7fc4b0f20d
tslavaa <70520545+tslavaa@users.noreply.github.com>
2021-02-27 23:49:57 +0600

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 
 exports.min = function min (array) {
-  return 0;
+    return array && array.length ? Math.min(...array) : 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+  return array && array.length ? Math.max(...array) : 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    return array && array.length ? array.reduce((acc, elem) => acc+elem, 0)/array.length : 0;
 }
